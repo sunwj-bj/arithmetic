2a9bbcaee64967257d465cfdf0d4c4dca87ff318
sunwj <934962711@qq.com>
2019-12-25 11:52:13 +0800

    动态规划算法

---
+++ src/main/java/com/economy/TradingStraregy.java
@@ -0,0 +1,23 @@
+package com.economy;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TradingStraregy {
+    /**
+     * 返回净值value增长特定百分比percent后的净值
+     * 保留四位小数，四舍五入
+     * @param value
+     * @param percent
+     * @return
+     */
+    public static BigDecimal computeValue(BigDecimal value,BigDecimal percent){
+        return value.multiply(percent).add(value).setScale(4, RoundingMode.HALF_DOWN);
+    }
+    public static void main(String[] args) {
+        String value="1.3686";
+        String percent="-0.05";
+        BigDecimal newValue = computeValue(new BigDecimal(value),new BigDecimal(percent));
+        System.out.println(newValue);
+    }
+}
