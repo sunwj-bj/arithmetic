f8eeb08113f6dde602ae49b1b315a6ed5a28d6ec
sunwj <934962711@qq.com>
2020-06-10 10:37:04 +0800

    添加了一个测试类

---
+++ src/main/java/com/other/ComprehensiveTest.java
@@ -0,0 +1,14 @@
+package com.other;
+
+/**
+ * @author sunwj
+ * 综合验证测试类
+ */
+public class ComprehensiveTest {
+    public static void main(String[] args) {
+        int i=1;
+        for (int j = 0; j <5 ; j++) {
+            System.out.println(++i);
+        }
+    }
+}
