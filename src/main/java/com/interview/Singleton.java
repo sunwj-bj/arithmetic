907edf33242cbe9a77ad4634f50a2f8bfbafa6d0
孙文杰 <sunwj25615@yunrong.cn>
2020-12-05 17:50:08 +0800

    单例优化详解

--- src/main/java/com/interview/Singleton.java
+++ src/main/java/com/interview/Singleton.java
@@ -14,6 +14,10 @@ public class Singleton {
         if (singleton == null) {
             synchronized (Singleton.class) {
                 if (singleton == null) {
+                    //这里可能发生指令重排序，正常初始化一个对象分为三个步骤：
+                    //1.初始化内存空间 2.将对象实例化 3.引用指向该内存空间
+                    //如果发生指令重排序，第二步和第三步调换执行顺序，别的线程来获取实例，
+                    // 判断引用不为空就会直接拿去用，但这时候对象可能并未初始化完成，可能会发生空指针。
                     singleton = new Singleton();
                 }
             }
