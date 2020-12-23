773b8bb57ebeccd46782d91f2a794ed19a144a54
孙文杰 <sunwj25615@yunrong.cn>
2020-12-23 20:06:54 +0800

    快速排序

--- src/main/java/com/sort/quick/QuickSort.java
+++ src/main/java/com/sort/quick/QuickSort.java
@@ -23,17 +23,20 @@ public class QuickSort {
         }
         if (l < r) {
             int i = l, j = r, x = s[l];
-            while (i < j)
-            {
-                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+            while (i < j){
+                while(i < j && s[j] >= x) { // 从右向左找第一个小于x的数
                     j--;
-                if(i < j)
+                }
+                if(i < j) {
                     s[i++] = s[j];
+                }
 
-                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                while(i < j && s[i] < x) { // 从左向右找第一个大于等于x的数
                     i++;
-                if(i < j)
+                }
+                if(i < j) {
                     s[j--] = s[i];
+                }
             }
             s[i] = x;
             quickSort(s, l, i - 1);
